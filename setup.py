342e6b4eed12ca1a6070ee778f6ab4de5fa24875
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-03-03 16:08:06 +0000

    Add minimal setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import sys
+from setuptools import setup
 
 if 'sdist' not in sys.argv:
     sys.exit(
         "\n*** Please install the `btrack` package "
         "instead of `napari-btrack-reader`) ***\n"
     )
+
+setup(
+    name="napari-btrack-reader"
+)
