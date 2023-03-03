269a0a528d4b7d384380e3cbf19d294df8f636b7
Patrick Roddy <patrickjamesroddy@gmail.com>
2023-03-03 16:25:55 +0000

    Fix PyPI deprecation (#7)
    Previous attempt failed #6. Have already pushed the tag. Can see it here
    https://pypi.org/project/napari-btrack-reader/.

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 import sys
+from setuptools import setup
 
-sys.exit(
-    "\n*** Please install the `btrack` package instead of `napari-btrack-reader`) ***\n"
+if 'sdist' not in sys.argv:
+    sys.exit(
+        "\n*** Please install the `btrack` package "
+        "instead of `napari-btrack-reader`) ***\n"
+    )
+
+description = (
+    "The functionality of this package has been integrated into "
+    "https://github.com/quantumjot/BayesianTracker."
+)
+
+setup(
+    name="napari-btrack-reader",
+    version="1.0.3",
+    description=description,
+    long_description=description,
 )
